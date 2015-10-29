abf10d455a59d74fb6451aaf9817798ad6f18958
blancopado <pablo.dominguezblanco@gmail.com>
2015-10-29 11:37:39 +0100

    Chapter 26: refactoring controllers

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -6,6 +6,20 @@ describe Game do
   let(:player2) { double :player }
   subject(:game) { described_class.new(player1, player2) }
 
+  describe 'initialize' do
+	  describe '#player_1' do
+	    it 'retrieves the first player' do
+	      expect(game.player1).to eq player1
+	    end
+	  end
+
+	  describe '#player_2' do
+	    it 'retrieves the second player' do
+	      expect(game.player2).to eq player2
+	    end
+	  end
+  end
+
   describe '#attack' do
     it 'damages the player' do
       expect(player1).to receive(:receive_damage)
